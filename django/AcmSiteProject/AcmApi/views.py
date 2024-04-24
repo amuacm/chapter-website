f2778576c8b0e26ecb98bbb1731add68fea4848e
Christopher Baier <baier.tech@outlook.com>
2024-04-23 21:07:05 -0400

    hello world

--- django/AcmSiteProject/AcmApi/views.py
+++ django/AcmSiteProject/AcmApi/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+# views
+
+@api_view(['GET'])
+def hello_world(request):
+    return Response({'message': 'Hello, world!'})
+
--- django/AcmSiteProject/AcmSiteProject/urls.py
+++ django/AcmSiteProject/AcmSiteProject/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('myapi.urls'))
+    path('api/', include('AcmApi.urls'))
 ]
