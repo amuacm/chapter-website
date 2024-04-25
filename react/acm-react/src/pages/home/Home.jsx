24ab0950ab91903660261927d99b83d58c6b6322
Cole Cantu <colecantu904@gmail.com>
2024-04-24 21:21:00 -0700

    messed with the text boxes on home, added hover affect

--- react/acm-react/src/pages/home/Home.jsx
+++ react/acm-react/src/pages/home/Home.jsx
@@ -1,29 +1,43 @@
 import React from 'react';
 import './HomeStyle.css'; 
 
+// Box react element, that is for housing a title and description.
+// all arguments are taken as strings
+function Box({ Title, Description, Link }) {
+  return (
+    <a className="boxed" href={Link}>
+      <h1>{Title}</h1>
+      <p>{Description} </p>
+    </a>
+  );
+}
+
 function Home() {
   return (
     <div classNameName="mainbody">
         <div className="splash"><img className="splash-image" src={require('./coding_splash.jpg')} alt="main screen" /></div>
 
         <div className="content-boxes">
-            <div className="boxed">
-                <h1>Resources</h1>
-                <p> This is the sentence. </p>
-            </div>
-            <div className="boxed">
-                <h1>Events</h1>
-                <p> This is the sentence. </p>
-            </div>
-            <div className="boxed">
-                <h1>Cool Stuff</h1>
-                <p> This is the sentence. </p>
-            </div>
-            <div className="boxed">
-                <h1>Join ACM!</h1>
-                <p> This is the sentence. </p>
-            </div>
-        
+            <Box
+            Title="Resources"
+            Description="Here you can find the links and information regarding Ave's CSCI program."
+            Link="https://saverio.carrd.co/"
+            />
+            <Box
+            Title="Events"
+            Description="Find upcoming events on campus and online! The ACM is always looking for ideas, so also feel free to submit your vision. "
+            Link="https://theimag.org/"
+            />
+            <Box
+            Title="Cool Stuff"
+            Description="If your looking for any insparation, or just want to see some interesting projects, explore the creativity of some of out peers."
+            Link="https://theimag.org/"
+            />
+            <Box
+            Title="Join ACM!"
+            Description="We are always looking for members! We feel that everyone can contribute in there own way"
+            Link="https://www.acm.org/"
+            />
         </div>
 
         <div>
