13ddf5ffc32e35f44cad5c09d7676ea4183cc5e7
Cole Cantu <colecantu904@gmail.com>
2024-04-25 23:29:55 -0700

    working on color theme and fonts

--- react/acm-react/src/pages/home/Home.jsx
+++ react/acm-react/src/pages/home/Home.jsx
@@ -5,17 +5,28 @@ import './HomeStyle.css';
 // all arguments are taken as strings
 function Box({ Title, Description, Link }) {
   return (
-    <a className="boxed" href={Link}>
+    <a className="boxed" href={Link} target="_blank" rel="noreferrer">
       <h1>{Title}</h1>
       <p>{Description} </p>
     </a>
   );
 }
 
+
 function Home() {
   return (
     <div className="mainbody">
-        <div className="splash"><img className="splash-image" src={require('./coding_splash.jpg')} alt="main screen" /></div>
+
+        <div className="splash"><h1>ACM@AMU</h1>
+          <div className="custom-shape-divider-bottom-1714112145">
+            <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+                <path d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z" className="shape-fill"></path>
+            </svg>
+        </div></div>
+
+      
+
+        {/* <div className="splash"><img className="splash-image" src={require('./coding_splash.jpg')} alt="main screen" /></div> */}
 
         <div className="content-boxes">
             <Box
