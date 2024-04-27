ff7ec3b87e80ec158c95a3c7c5e052faf17d0f45
Christopher Baier <baier.tech@outlook.com>
2024-04-27 18:54:15 -0400

    postgres configured in settings

--- django/AcmSiteProject/AcmSiteProject/settings.py
+++ django/AcmSiteProject/AcmSiteProject/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'AcmSiteProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        "NAME": "acmsite",
+        "USER": "postgres",
+        "PASSWORD": "root",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
 
