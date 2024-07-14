3da611c90ac9a92862864eba2d6ae10ad627a4e4
Christopher Baier <baier.tech@outlook.com>
2024-07-14 14:02:21 -0400

    restructured for react only stack

--- AcmSiteProject/passenger_wsgi.py
+++
@@ -1,7 +0,0 @@
-import os
-import sys
-from AcmSiteProject.wsgi import application
-
-sys.path.insert(0, os.path.dirname(__file__))
-
-environ = os.environ.setdefault("DJANGO_SETTINGS_MODULE", "AcmSiteProject.settings")
