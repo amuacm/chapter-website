476bc4ba6d588a03014bc16e4df55cc0ee12e5bf
Christopher Baier <baier.tech@outlook.com>
2024-07-23 18:57:33 -0400

    added nodejs backend server

---
+++ backend/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+app.use((req, res) => {
+    res.status(200).send('Hello, world!');
+});
+// Start the server
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}`);
+    console.log('Press Ctrl+C to quit.');
+});
